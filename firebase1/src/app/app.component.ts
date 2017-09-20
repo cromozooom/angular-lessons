265c5b6b5ea5eb6c78d4c788120eb7cd8001554b
cromozooom <cromozooom@gmail.com>
2017-09-20 08:21:29 +0100

    add lista of businesses

--- firebase1/src/app/app.component.ts
+++ firebase1/src/app/app.component.ts
@@ -6,26 +6,37 @@ import {Business} from './Business';
 import {Category} from './Category';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [FirebaseService]
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	providers: [FirebaseService]
 })
 export class AppComponent implements OnInit{
-  businesses:Business[];
-  categories:Category[];
-  
-  constructor(private _firebaseService:FirebaseService){}
+	businesses:Business[];
+	categories:Category[];
+	appState:string;
+	activeKey:string;
 
-  ngOnInit(){
-    this._firebaseService.getBusinesses().subscribe(businesses => {
-      console.log(businesses);
-      this.businesses = businesses;
-    });
 
-    this._firebaseService.getCategories().subscribe(categories => {
-      console.log(categories);
-      this.categories = categories;
-    });
-  }
+	constructor(private _firebaseService:FirebaseService){}
+
+	ngOnInit(){
+		this._firebaseService.getBusinesses().subscribe(businesses => {
+			console.log(businesses);
+			this.businesses = businesses;
+		});
+
+		this._firebaseService.getCategories().subscribe(categories => {
+			console.log(categories);
+			this.categories = categories;
+		});
+	}
+	changeState(state, key){
+		console.log("changing state to" + state)
+		if(key){
+			console.log("changing key to" + key)
+			this.activeKey = key;
+		}
+		this.appState = state
+	}
 }
