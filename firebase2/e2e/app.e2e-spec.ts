f5ab9f92110839f9ef268990e44fd12bde739f71
cromozooom <cromozooom@gmail.com>
2017-09-25 17:00:21 +0100

    add angular 4 enviroment to firebase2

--- firebase2/e2e/app.e2e-spec.ts
+++ firebase2/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { Firebase2Page } from './app.po';
+import { AppPage } from './app.po';
 
-describe('firebase2 App', function() {
-  let page: Firebase2Page;
+describe('firebase2 App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new Firebase2Page();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
